b08915e22c996a6fefbf48d02c9afb25faa79177
aziz <b3thr2@gmai.com>
2026-02-06 23:37:50 +0300

    Initial Commit

---
+++ AzChat/AzChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzChatApp.swift
+//  AzChat
+//
+//  Created by Amal  on 18/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct AzChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AzChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AzChat
+//
+//  Created by Amal  on 18/08/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
